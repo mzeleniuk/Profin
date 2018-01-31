dee91ac7c765a2065e2a9e0c32573825ce0e933d
Michael Zelenyuk <zell.xp@gmail.com>
2018-01-31 16:38:13 +0200

    Create a Search Page component

--- App.js
+++ App.js
@@ -1,29 +1,8 @@
 'use strict';
 
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-class SearchPage extends Component<{}> {
-  static navigationOptions = {
-    title: 'Profin'
-  };
-
-  render() {
-    return (
-      <Text style={styles.description}>Search for houses to buy</Text>
-    );
-  };
-}
-
-const styles = StyleSheet.create({
-  description: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 65
-  }
-});
+import SearchPage from './components/SearchPage';
 
 const App = StackNavigator({
   Home: { screen: SearchPage }
