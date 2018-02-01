470fc09136d3e6fd1e6b33deb812a0b3b13cfc9c
Michael Zelenyuk <zell.xp@gmail.com>
2018-02-01 16:08:53 +0200

    Add search form to Search Page component

--- components/SearchPage.js
+++ components/SearchPage.js
@@ -26,6 +26,13 @@ export default class SearchPage extends Component<{}> {
         <Text style={styles.description}>
           Search by place-name or postcode
         </Text>
+
+        <View style={styles.flowRight}>
+          <TextInput underlineColorAndroid={'transparent'}
+                     style={styles.searchInput}
+                     placeholder='Search via name or postcode'/>
+          <Button onPress={() => {}} color='#48BBEC' title='Search'/>
+        </View>
       </View>
     );
   }
@@ -42,5 +49,21 @@ const styles = StyleSheet.create({
     padding: 30,
     marginTop: 65,
     alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flexGrow: 1,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 2,
+    color: '#48BBEC'
   }
 });
